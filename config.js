c67511a63fd5459566d45de735b296282785bdfd
Sezynex <100211433+Sezynex@users.noreply.github.com>
2024-09-02 23:32:38 +0300

    upload1

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: '',
+	imageBackground: true,
+	openInNewTab: true,
+	twelveHourFormat: false,
+
+	// Greetings
+	greetingMorning: 'Günaydın!',
+	greetingAfternoon: 'İyi günler.',
+	greetingEvening: 'İyi akşamlar',
+	greetingNight: 'Yat artık!',
+
+	// Layout
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: '5a3ae9eb5a361e6941474774b0da1e69', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'C', // 'F', 'C'
+	language: 'tr', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '41.008240',
+	defaultLongitude: '28.978359',
+
+	// Autochange
+	autoChangeTheme: true,
+
+	// Autochange by OS
+	changeThemeByOS: true,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Youtube',
+			icon: 'youtube',
+			link: 'https://youtube.com',
+		},
+		{
+			id: '2',
+			name: 'Twitch',
+			icon: 'twitch',
+			link: 'https://twitch.tv',
+		},
+		{
+			id: '3',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://outlook.com',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '5',
+			name: 'Steam',
+			icon: 'gamepad-2',
+			link: 'https://steamcommunity.com/id/sezynex/',
+		},
+		{
+			id: '6',
+			name: 'Reddit',
+			icon: 'bot',
+			link: 'https://reddit.com',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'youtube',
+			id: '1',
+			links: [
+				{
+					name: 'Daha sonra izle',
+					link: 'https://www.youtube.com/playlist?list=WL',
+				},
+				{
+					name: 'Geçmiş',
+					link: 'https://www.youtube.com/feed/history',
+				},
+				{
+					name: 'NPC',
+					link: 'https://www.youtube.com/playlist?list=PLu-wt498QPV05MxAELFHRXKUvruDh3BQt',
+				},
+				{
+					name: 'Kanal',
+					link: 'https://www.youtube.com/@sezynex',
+				},
+			],
+		},
+		{
+			icon: 'computer',
+			id: '2',
+			links: [
+				{
+					name: 'GitHub',
+					link: 'https://github.com/Sezynex',
+				},
+				{
+					name: 'LMS',
+					link: 'https://ue.gedik.edu.tr',
+				},
+				{
+					name: 'OBS',
+					link: 'https://obs.gedik.edu.tr',
+				},
+				{
+					name: 'Whatsapp',
+					link: 'https://web.whatsapp.com',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'binary',
+			id: '1',
+			links: [
+				{
+					name: 'Spotify',
+					link: 'https://www.spotify.com',
+				},
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com',
+				},
+				{
+					name: 'Hashnode',
+					link: 'https://www.hashnode.com',
+				},
+				{
+					name: 'Pocket',
+					link: 'https://www.pocket.com',
+				},
+			],
+		},
+		{
+			icon: 'github',
+			id: '2',
+			links: [
+				{
+					name: 'Front',
+					link: 'https://www.reddit.com/r/Frontend/',
+				},
+				{
+					name: 'Rust',
+					link: 'https://www.reddit.com/r/rust/',
+				},
+				{
+					name: 'Go',
+					link: 'https://www.reddit.com/r/golang/',
+				},
+				{
+					name: 'Repos',
+					link: 'https://github.com/migueravila',
+				},
+			],
+		},
+	],
+};
